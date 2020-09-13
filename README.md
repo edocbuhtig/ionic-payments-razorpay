15212956775f9e3bed8f0efabd67a20ea6e46063
Sunil Kumar <47194793+consultantsk@users.noreply.github.com>
2020-09-13 18:33:55 +0530

    razorpay integration

---
+++ README.md
@@ -0,0 +1,21 @@
+## Ionic Payments Using Razorpay Payment Gateway
+
+## Razorpay Cordova Plugin
+
+```
+ionic cordova plugin add com.razorpay.cordova --save
+```
+
+## Screenshots
+
+Checkout Screen  |  Payments Banks |  Status Screen 
+:-------------------------:|:-------------------------:|:-------------------------:
+![](screenshots/payments-screen.png)  |  ![](screenshots/payments-banks-screen.png) |  ![](screenshots/status-screen.png) 
+
+
+## Running The App
+
+```
+ionic cordova run browser
+```
+
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+declare var RazorpayCheckout: any;
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -8,5 +10,40 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   constructor() {}
+
+  pay(){
+    let options = {
+      description: 'Ionic Razorpay Payments Demo',
+      currency: "INR", // your 3 letter currency code
+      key: "", // your test/live Key Id from Razorpay settings dashboard
+      amount: 100, // Payment amount 
+      name: 'Ionic Razorpay Demo', 
+      prefill: {
+        email: 'demopay@sunilk.work',
+        contact: '123456789',
+        name: 'Ionic Razorpay Demo'
+      },
+      theme: {
+        color: '#fefd23'
+      },
+      modal: {
+        ondismiss: function () {
+          alert('dismissed')
+        }
+      }
+    };
+
+    let successCallback = function (payment_id) {
+      console.log('payment_id: ' + payment_id);
+      //if payment id is received (success), here you can update your database like firebase, or mariadb etc.
+    };
+
+    let cancelCallback = function (error) {
+      console.log(error.description + ' (Error ' + error.code + ')');
+      //inform the user to re-initiate the payment based or error codes.
+    };
+
+    RazorpayCheckout.open(options, successCallback, cancelCallback);
+  }
 
 }
